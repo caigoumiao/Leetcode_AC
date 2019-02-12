7769936ac7bbc0defc085f9ae99fbd2701300536
miaoqiao <miaoqiao0517@163.com>
2019-02-12 10:41:10 +0800

    add 794[unsolved]

---
+++ src/_751_to_800/_794.java
@@ -0,0 +1,15 @@
+package _751_to_800;
+
+/**
+ * @author larry miao
+ * @date 2018-02-12
+ * @url https://leetcode-cn.com/problems/valid-tic-tac-toe-state/
+ * @title 794. 有效的井字游戏
+ */
+public class _794
+{
+    public boolean validTicTacToe(String[] board)
+    {
+        return false;
+    }
+}
